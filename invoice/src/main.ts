d3c129bb8077296a1753cb98b9cdc288c6013483
Bruno Petrolini <brunopetrolini@hotmail.com>
2022-12-12 08:51:02 -0300

    feat: adds http client axios implementation

--- invoice/src/main.ts
+++ invoice/src/main.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { AxiosAdapter } from "./axios-adapter";
 import { CalculateInvoice } from "./calculate-invoice";
 import { CurrencyGatewayHttp } from "./currency-gateway-http";
 import { TransactionDAODatabase } from "./transaction-dao-database";
@@ -6,7 +7,9 @@ import { TransactionDAODatabase } from "./transaction-dao-database";
 const app = express();
 app.get("/cards/:cardNumber/invoices", async (request, response) => {
   const transactionDAO = new TransactionDAODatabase();
-  const currencyGateway = new CurrencyGatewayHttp();
+  const httpClient = new AxiosAdapter();
+  const baseUrl = "http://localhost:3001";
+  const currencyGateway = new CurrencyGatewayHttp(httpClient, baseUrl);
   const calculateInvoice = new CalculateInvoice(transactionDAO, currencyGateway);
   const total = await calculateInvoice.execute(request.params.cardNumber);
   response.json({ total });
