c8b15d122aba74104d933ab0e0811f4e1151ec63
Bruno Petrolini <brunopetrolini@hotmail.com>
2022-12-12 08:19:48 -0300

    feat: adds real dollar exchange rate

--- currency/src/main.ts
+++ currency/src/main.ts
@@ -1,7 +1,11 @@
+import axios from "axios";
 import express from "express";
 
 const app = express();
-app.get("/currencies", (request, response) => {
-  response.json({ usd: 5 });
+app.get("/currencies", async (request, response) => {
+  const result = await axios.get("https://economia.awesomeapi.com.br/last/USD-BRL");
+  const output = result.data;
+
+  response.json({ usd: output.USDBRL.bid });
 });
 app.listen(3001);
